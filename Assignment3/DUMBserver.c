e4d0692b60421e7a75aac93d164e56cefd105e09
Ilana Zane <46580323+ilanazane@users.noreply.github.com>
2019-12-11 23:06:35 -0500

    Update DUMBserver.c

--- Assignment3/DUMBserver.c
+++ Assignment3/DUMBserver.c
@@ -8,6 +8,51 @@
 #include <unistd.h> // for close
 #include<pthread.h>
 
+typedef struct Node Node; 
+
+struct Node{
+ char* name; 
+ struct Node *next; 
+
+};
+
+Node *head=NULL; 
+Node *curr=NULL; 
+Node *data=wordtwo;
+
+Node *addToBox(char *name){
+  Node *temp=head; 
+  while(temp){
+    temp=temp->next; 
+  }
+  Node *ptr=malloc(sizeof(Node)); 
+  ptr->name=malloc(stlen(name)+1); 
+  strcpy(ptr->name,name); 
+  prt->next=NULL;
+
+  if(1){
+    curr->next=ptr; 
+    curr=ptr; 
+  }else{
+    ptr->next=head; 
+    head=ptr; 
+  }
+  return ptr; 
+}
+
+Node *check(char *name){
+  Node *ptr=head; 
+  while(ptr!=NULL){
+   int compare=strcmp(ptr,name); 
+   if(compare==0){
+     return 1; //exists
+   }else{
+     return 0; //doesnt exist 
+   }
+  }
+}
+
+
 char client_message[2000];//string 
 char buffer[1024];
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
@@ -19,7 +64,8 @@ void * socketThread(void *arg)
   	
   // Send message to the client socket 
   pthread_mutex_lock(&lock);
-  char *message = malloc(sizeof(client_message)+20);
+  char *message = malloc(sizeof(client_message)+35);
+  int exists=0; 
   //HELLO 
   recv(newSocket , client_message , 2000 , 0);
   	int correct=strcmp(client_message,"HELLO"); 
@@ -36,26 +82,28 @@ void * socketThread(void *arg)
 	  if(correct2==0){
 	    close(newSocket); 
 	  }
+        //CREAT
         int i=0; 
  	int length=strlen(client_message); 
 	char wordOne[4]; 
-	char wordTwo[]=malloc(sizeof(char)); 
+	char wordtwo[]=malloc(sizeof(char)); 
 	for(i=0;i<5;i++){
 	 wordOne=client_message[i];
 	}
 	
-	//CREAT
 	int correct3=strcmp(wordOne,"CREAT"); 
 	if(correct3==0){
 	  if(client_message[6]==" "){
 	    int j=0; 
 	    for(j=6;j<length;j++){
-		wordTwo=client_message[j]; 
+		wordtwo=client_message[j]; 
 	    }
-	    int length2=strlen(wordTwo); 
+	    int length2=strlen(wordtwo); 
 	    if(length2>=5 && length2<=25){
-		int alpha=isalpha(wordTwo[0]); 
+		int alpha=isalpha(wordtwo[0]); 
 		if(alpha!=0){
+		  addToBox(wordtwo); //add to message box 
+                  exists=1; 
 		  strcpy(buffer, "OK!"); 
 		  send(newSocket,buffer,13,0); 
 		}
@@ -71,7 +119,78 @@ void * socketThread(void *arg)
 		strcpy(buffer, "ER:WHAT?");
 		send(newSocket, buffer, 13,0);  
        }
-     
+       if(exists==1){
+         strcpy(buffer, "ER:EXIST");
+	 send(newSocket, buffer, 13,0);
+       }
+       
+
+       //OPNBX
+        int m=0;
+        int opened=0; 
+ 	int length2=strlen(client_message); 
+	char wordone[4]; 
+	char wordtwo[]=malloc(sizeof(char)); 
+	for(m=0;m<5;m++){
+	 wordone=client_message[m];
+	}
+
+       	int correct5=strcmp(wordone,"OPNBX"); 
+	if(correct5==0){
+	  if(client_message[6]==" "){
+	   int j=0; 
+	    for(j=6;j<length2;j++){
+		wordtwo=client_message[j]; 
+	    }
+            //check if in box 
+   	     exists=check(wordtwo); 
+             if(exists==1){
+		strcpy(buffer,"OK!");
+                opened=1; 
+ 		send(newSocket, buffer, 13,0); 
+	     }else{
+               strcpy(buffer,"ER:NEXST"); 
+	       send(newSocket, buffer, 13,0); 
+	     }
+          }else{
+          strcpy(buffer,"ER:WHAT?"); 
+	       send(newSocket, buffer, 13,0); 
+	  }
+        }else{
+	   strcpy(buffer,"ER:WHAT?"); 
+	   send(newSocket, buffer, 13,0); 
+	}
+        if(exists==1){
+	  strcpy(buffer,"ER:OPEND"); 
+	  send(newSocket, buffer, 13,0); 
+	}
+
+	
+/*
+      //NXTMG 
+        int a=0;
+ 	int length3=strlen(client_message); 
+	char Wordone[4]; 
+	char wordtwo[]=malloc(sizeof(char)); 
+	for(a=0;a<5;a++){
+	 Wordone=client_message[a];
+	}
+
+       	int correct6=strcmp(Wordone,"NXTMG"); 
+	if(correct5==0){
+	 if(opened==1){
+           while(temp!=NULL){
+	     Node *temp=head; 
+             int msgSize=0; 
+	     msgSize=strlen(head->data); 
+             strcat(message,"OK!"); 
+  	     strcat(message,msgSize); 
+             strcat(
+           }//while
+	 }//if
+        }//if
+*/
+      
 	  
          
 	
